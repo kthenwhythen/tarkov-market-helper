2e58a160a8f9c1c263866c6a0e10481e23c3831b
null-architect <stry.bout.eyes@gmail.com>
2021-02-20 16:29:13 +0900

    Covered with comments main_frame.py

--- main_frame.py
+++ main_frame.py
@@ -1,79 +1,97 @@
 import wx
-import mouse
-import time
-import threading
+from mouse import get_position
+from time import sleep
+from threading import Thread
+from pandas import DataFrame
 from scan import Scan
 from items import Items
-import pandas
 
 
 class MainFrame(wx.Frame):
     def __init__(self, hash_lang, fps):
+        """
+        Init of MainFrame that show item price when mouse is pointing on an item
+
+        Arguments:
+        hash_lang -- chosen lang of game client
+        fps -- how often will be updated MainFrame
+        """
         # Set style and options of Frame
         style = (wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR | wx.SIMPLE_BORDER)
         super().__init__(None, title='Tarkov Market Helper', size=(92, 76), style=style)
-
-        # Style settings
         self.panel = wx.Panel(self)
         self.SetTransparent(220)
         self.SetBackgroundColour('black')
 
-        # Items init
+        # Items init with chosen lang
         self.items = Items(hash_lang)
 
-        # Something with item
+        # Remember prev item hash to compare with current item hash (for optimization)
         self.previous_item_hash = ''
-        self.item = pandas.DataFrame()
+
+        # Starting value for item values
+        self.item = DataFrame()
         self.item_state = 'No item'
 
-        # App settings
+        # MainFrame update time with fps value that get from settings
         self.update_time = 1 / fps
 
-        # Starting label placeholder
-        self.min_price_text = wx.StaticText(self.panel, label='')
-        self.slot_price_text = wx.StaticText(self.panel, label='')
-        self.trader_price_text = wx.StaticText(self.panel, label='')
-
         # Init UI
+        self.min_price_text = None
+        self.slot_price_text = None
+        self.trader_price_text = None
         self.init_ui()
 
         # Starting thread for positioning frame
         self.thread_is_on = True
         self.turn_thread()
 
-    def turn_thread(self):  # Changing thread bool and then start thread
+    def turn_thread(self):
+        """
+        Changing thread bool and then start thread
+        """
         self.thread_is_on = not self.thread_is_on
 
         if self.thread_is_on:
-            thread = threading.Thread(target=self.thread_frames)
+            thread = Thread(target=self.thread_frames)
             thread.start()
 
     def thread_frames(self):
+        """
+        Thread body that looping while is on
+        """
         while self.thread_is_on:
             self.update_frame()
-            time.sleep(self.update_time)
+            sleep(self.update_time)
+
         self.Show(False)
 
-    def update_frame(self):  # Updating frame with position, item hash and UI
-        # Update position
-        pos_x, pos_y = mouse.get_position()
+    def update_frame(self):
+        """
+        Update frame with position, item hash and UI
+        """
+        # Update mouse position
+        pos_x, pos_y = get_position()
         wx.CallAfter(self.Move, wx.Point(pos_x + 10, pos_y + 20))
 
-        # Auto scan (hitting performance)
-        # if self.mode == 'auto' and self.mode == 'turn':
+        # Scan item that set item_hash and item_state
         self.scan_item()
 
-        # Update item info
-        if not self.item.empty and self.item_state == "Item data found" and self.previous_item_hash != self.item[f"Hash {self.items.hash_lang}"].iloc[0]:
+        # Update item info in UI
+        if not self.item.empty and self.item_state == "Item data found" \
+                and self.previous_item_hash != self.item[f"Hash {self.items.hash_lang}"].iloc[0]:
             self.previous_item_hash = self.item[f"Hash {self.items.hash_lang}"].iloc[0]
             self.update_ui()
-            # print(self.item["Hash EN"].item())
+
         elif self.item.empty and self.item_state == "Item data not found" \
                 and self.previous_item_hash != "":
             self.previous_item_hash = ""
             self.update_ui()
 
     def init_ui(self):
+        """
+        Init UI when app start
+        """
         hbox = wx.BoxSizer()
         fb = wx.FlexGridSizer(3, 2, 6, 4)
 
@@ -95,43 +113,51 @@ class MainFrame(wx.Frame):
         self.trader_price_text = wx.StaticText(self.panel, label=f'₽ N/A')
         self.trader_price_text.SetForegroundColour((240, 226, 42))
 
-        # help_title = wx.StaticText(self.panel, label="(F1)", style=wx.ALIGN_RIGHT)
-        # help_title.SetForegroundColour((160, 160, 170))
-
         fb.AddMany([min_price_title, self.min_price_text,
                     slot_price_title, self.slot_price_text,
                     trader_price_title, self.trader_price_text,
                     ])
-
-        fb.AddGrowableCol(1, 1)
 
         hbox.Add(fb, proportion=1, flag=wx.EXPAND | wx.ALL, border=6)
         self.panel.SetSizer(hbox)
         self.Layout()
 
     def update_ui(self):
-        if self.item_state == "Item data found":
+        """
+        Update UI when thread looping
+        """
+        if self.item_state == 'Item data found':
             self.min_price_text.SetLabel(f"₽{self.item['Lowest price'].iloc[0]}")
             self.slot_price_text.SetLabel(f"₽{self.item['Price per slot'].iloc[0]}")
             self.trader_price_text.SetLabel(f"₽{self.item['Trader price'].iloc[0]}")
-        elif self.item_state == "Item data not found":
-            self.min_price_text.SetLabel("₽ N/A")
-            self.slot_price_text.SetLabel("₽ N/A")
-            self.trader_price_text.SetLabel("₽ N/A")
+
+        elif self.item_state == 'Item data not found':
+            self.min_price_text.SetLabel('N/A')
+            self.slot_price_text.SetLabel('N/A')
+            self.trader_price_text.SetLabel('N/A')
 
     def scan_item(self):
+        """
+        Scan item when thread looping and set item_hash, item_state
+        """
         try:
             self.item, self.item_state = self.items.find(Scan().item_hash)
+
         except AttributeError as error:
             print(error)
             self.item, self.item_state = self.items.find('')
 
-        if not self.item.empty and self.item_state == "Item data found" and self.thread_is_on:
+        if not self.item.empty and self.item_state == 'Item data found' and self.thread_is_on:
             self.Show(True)
-        elif self.item.empty and self.item_state == "Item data not found" and self.thread_is_on:
+
+        elif self.item.empty and self.item_state == 'Item data not found' and self.thread_is_on:
             self.Show(True)
+
         else:
             self.Show(False)
 
     def note_item(self):
-        print(f'F4: {self.item_state}: {Scan().item_hash}')
+        """
+        Used for debug purpose
+        """
+        print(f'Debug Hash: {self.item_state}: {Scan().item_hash}')
